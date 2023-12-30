c12dfa60006d3a0734025239703f350fcf53affc
Ushkevich Oleg <olegserbat@yandex.ru>
2023-12-30 15:46:48 +0300

    fix the autoload connection to files, end gcd game

--- src/GcdGame.php
+++ src/GcdGame.php
@@ -3,8 +3,28 @@
 namespace Braingames\GcdGame;
 
 use function cli\line;
+use function cli\prompt;
 use BrainGames\Cli;
 
 function gcdGame(){
     $name = Cli\askName();
+    line("Find the greatest common divisor of given numbers.");
+    for ($i = 0; $i < 3; $i++) {
+        $firstNumber = rand(1, 25);
+        $secondNumber = rand(1, 25);
+        line("Question: {$firstNumber} {$secondNumber}");
+        $correctAnswer = gmp_gcd($firstNumber, $secondNumber);
+        $answer = prompt("Your answer");
+        if ($answer == $correctAnswer){
+            line("Correct!");
+            if ($i == 2){
+                line("Congratulations, {$name}!");
+            }
+        }
+        else{
+            line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.");
+            line("Let's try again, Sam!");
+            break;
+        }
+    }
 }
